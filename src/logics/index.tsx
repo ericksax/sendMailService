c4a3bf2ad30f9b5d5ff3e244e8b5a067666e6742
Erick de Freitas Silva <TI@TI05.mac.local>
2023-08-04 18:11:21 -0300

    fix insert correct env variable

--- src/logics/index.tsx
+++ src/logics/index.tsx
@@ -14,7 +14,7 @@ const sendMail = async (req: Request, res: Response) => {
 
   const transporter = createTransport({
     host: process.env.MAIL_HOST,
-    port: Number(process.env.DB_PORT),
+    port: Number(process.env.MAIL_PORT),
     auth: {
       user: process.env.MAIL_USER,
       pass: process.env.MAIL_PASS,
